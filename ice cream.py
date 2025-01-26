0ef34e56e8b1a1d44fe0b44a485a846d1f4c75a9
nastya752 <nastyckat984@gmail.com>
2025-01-26 03:42:36 -0800

    ice cream program

---
+++ ice cream.py
@@ -0,0 +1,10 @@
+from tkinter import*
+app=Tk()
+count=[0]
+app.geometry("1000x500+500+200")
+app.title("first window")
+app["bg"]="#e5a3e5"
+ice=Label(text="ice cream", font=("ice cream", 24), bg="light blue")
+ice.place(x=400, y=50)
+app.mainloop()
+gggggg
